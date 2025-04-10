e10d099ee47fbe336bf752e6ef31d3fffbf57936
Kimeunho0710 <keh710427@gmail.com>
2025-04-10 14:27:49 +0900

    폴더수정

---
+++ backend/src/test/java/com/backend/careerecipe/CareerecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.careerecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
