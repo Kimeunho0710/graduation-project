6407c742503893f8e95934a56c27295c710610c5
yeonssseo <dltj2541@naver.com>
2025-05-14 23:38:54 +0900

    전공 추천 페이지 드롭다운 완성, 필터링은 안함함

--- frontend/graduation-project/src/pages/MajorSelectPage.js
+++ frontend/graduation-project/src/pages/MajorSelectPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import '../pages-css/MajorSelectPage.css';
@@ -7,18 +7,44 @@ function MajorSelectPage() {
   const location = useLocation();
   const jobId = location.state?.jobId;
 
+  const [showDeptList, setShowDeptList] = useState(false);
+  const [showGradeList, setShowGradeList] = useState(false);
+  const [showSemesterList, setShowSemesterList] = useState(false);
+  const [selectedDept, setSelectedDept] = useState('학과');
+  const [selectedGrade, setSelectedGrade] = useState('학년');
+  const [selectedSemester, setSelectedSemester] = useState('학기');
   const [subjects, setSubjects] = useState([]);
   const [favorites, setFavorites] = useState({});
 
-  useEffect(() => {
-    if (jobId) {
-      axios
-        .get(`/api/jobs/${jobId}/subjects`)
-        .then((res) => setSubjects(res.data))
-        .catch((err) => console.error('전공 과목 추천 조회 실패:', err));
-    }
-  }, [jobId]);
+  const departments = [
+    '전체',
+    '컴퓨터SW학과',
+    '미디어SW학과',
+    '정보통신학과',
+    '정보보호학과',
+    '데이터과학부',
+  ];
 
+  const grades = ['전체', '1학년', '2학년', '3학년', '4학년'];
+
+  const semesters = ['전체', '1학기', '2학기'];
+
+  const handleDeptSelect = (dept) => {
+    setSelectedDept(dept);
+    setShowDeptList(false);
+  };
+
+  const handleGradeSelect = (grade) => {
+    setSelectedGrade(grade);
+    setShowGradeList(false);
+  };
+
+  const handleSemesterSelect = (sem) => {
+    setSelectedSemester(sem);
+    setShowSemesterList(false);
+  };
+
+  // 즐겨찾기 버튼 효과
   const toggleFavorite = (index) => {
     setFavorites((prev) => ({ ...prev, [index]: !prev[index] }));
   };
@@ -26,18 +52,73 @@ function MajorSelectPage() {
   return (
     <div className="MajorSelectPage">
       <div className="FilterBar">
-        <button className="FilterButton">
-          <div>학과</div>
-          <div>⬇</div>
-        </button>
-        <button className="FilterButton">
-          <div>학년</div>
-          <div>⬇</div>
-        </button>
-        <button className="FilterButton">
-          <div>학기</div>
-          <div>⬇</div>
-        </button>
+        <div className="FilterDropdown">
+          <button
+            className="FilterButton"
+            onClick={() => setShowDeptList(!showDeptList)}
+          >
+            <div>{selectedDept}</div>
+            <div>⬇</div>
+          </button>
+          {showDeptList && (
+            <div className="Dropdown">
+              {departments.map((dept, i) => (
+                <div
+                  className="DropdownItem"
+                  key={i}
+                  onClick={() => handleDeptSelect(dept)}
+                >
+                  {dept}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+
+        <div className="FilterDropdown">
+          <button
+            className="FilterButton"
+            onClick={() => setShowGradeList(!showGradeList)}
+          >
+            <div>{selectedGrade}</div>
+            <div>⬇</div>
+          </button>
+          {showGradeList && (
+            <div className="Dropdown">
+              {grades.map((grade, i) => (
+                <div
+                  className="DropdownItem"
+                  key={i}
+                  onClick={() => handleGradeSelect(grade)}
+                >
+                  {grade}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="FilterDropdown">
+          <button
+            className="FilterButton"
+            onClick={() => setShowSemesterList(!showSemesterList)}
+          >
+            <div>{selectedSemester}</div>
+            <div>⬇</div>
+          </button>
+          {showSemesterList && (
+            <div className="Dropdown">
+              {semesters.map((sem, i) => (
+                <div
+                  className="DropdownItem"
+                  key={i}
+                  onClick={() => handleSemesterSelect(sem)}
+                >
+                  {sem}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
 
       <div className="SubjectList">
